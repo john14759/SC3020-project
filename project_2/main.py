a7cc2e52d2aeec9dca5e9ae465743c08710f2a36
john14759 <john14759@gmail.com>
2023-11-04 23:20:03 +0800

    Changed formatting of digraph + added legend

--- project_2/main.py
+++ project_2/main.py
@@ -30,6 +30,24 @@ def execute_sql_query():
     except Exception as e:
         result_label.config(text=f"Error: {str(e)}")
 
+    # Create a legend canvas
+    legend_canvas = tk.Canvas(window, width=600, height=100)
+    legend_canvas.pack(padx=10, pady=10)
+
+    # Legend items
+    legend_items = [
+        {"text": "start-up cost is the estimated Postgres computational units to start up a node to start process the query."},
+        {"text": "total cost is the estimated Postgres computational units to finish process the query and return results"},
+        {"text": "Shared Hit Blocks: number of shared blocks read into cache"},
+        {"text": "Local Hit Blocks: number of local blocks read into cache"},
+    ]
+
+    # Create four legend items
+    for i, item in enumerate(legend_items):
+        y_offset = i * 40  # Adjust Y-coordinate to separate items
+        legend_canvas.create_oval(5, 5 + y_offset, 15, 15 + y_offset, fill="black")
+        legend_canvas.create_text(20, 10 + y_offset, text=item["text"], anchor="w")
+
 # Create the main window
 window = tk.Tk()
 window.title("SQL Query Executor")
--- project_2/test.py
+++ project_2/test.py
@@ -31,7 +31,8 @@ def connection_to_db():
         print(f"Error connecting to the database: {e}")
 
 def get_qep_image(cursor, query):
-    cursor.execute(f"EXPLAIN (FORMAT JSON) {query}")
+    explain_query = f"EXPLAIN (ANALYZE, BUFFERS, FORMAT JSON) {query}"
+    cursor.execute(explain_query)
     qep_json = cursor.fetchone()[0][0]
     print(qep_json)
 
@@ -43,20 +44,40 @@ def get_qep_image(cursor, query):
     else:
         return None
 
-def add_nodes(dot, plan):
+def add_nodes(dot, plan, parent_id=None, node_id=0):
     if "Node Type" in plan:
-        # Construct a label with the desired information
-        node_label = f"{plan['Node Type']}\nStartup Cost: {plan.get('Startup Cost', 'N/A')}\nTotal Cost: {plan.get('Total Cost', 'N/A')}"
+        # Extract information from the JSON data
+        node_type = plan["Node Type"]
+        startup_cost = plan.get('Startup Cost', 'N/A')
+        total_cost = plan.get('Total Cost', 'N/A')
+        relation_name = plan.get('Relation Name', 'N/A')
+        index_name = plan.get('Index Name', 'N/A')
+        shared_hit_blocks = plan.get('Shared Hit Blocks', 'N/A')
 
-        # Add the node to the graph
-        dot.node(node_label, label=node_label)
+        # Construct the label with relevant information
+        label = f"{node_type}\nRelation Name: {relation_name}\nIndex Name: {index_name}\nStartup Cost: {startup_cost}\nTotal Cost: {total_cost}\nShared Hit Blocks: {shared_hit_blocks}"
+        
+        # Define a unique identifier for the node
+        node_name = f"node{node_id}"
+        dot.node(node_name, label=label)
 
+        # If there's a parent, add an edge from this node to the parent (reverse order)
+        if parent_id is not None:
+            dot.edge(node_name, parent_id)
+
+        # Recurse for children if they exist
         if "Plans" in plan:
             for subplan in plan["Plans"]:
-                # Recursively add the subplan to the graph
-                add_nodes(dot, subplan)
+                node_id += 1
+                # Recursively add the subplan to the graph with incremented node ID
+                node_id = add_nodes(dot, subplan, parent_id=node_name, node_id=node_id)
 
-                # Connect the node to its subplan
-                dot.edge(node_label, f"{subplan['Node Type']}\nStartup Cost: {subplan.get('Startup Cost', 'N/A')}\nTotal Cost: {subplan.get('Total Cost', 'N/A')}")
+    # Return the next available node_id
+    return node_id
+
+
+
+
+
 
 
