818c15da1cc8515d66d67c6a9abea8b6f5d818b8
john14759 <john14759@gmail.com>
2023-11-14 20:55:34 +0800

    Fixed issue with legend and made GUI fullscreen

--- project_2/main.py
+++ project_2/main.py
@@ -2,8 +2,32 @@ import tkinter as tk
 from PIL import Image, ImageTk
 from test import connection_to_db, get_qep_image
 
-# Define a function to execute the SQL query and display the QEP image
+# Create the main window
+window = tk.Tk()
+window.title("SQL Query Executor")
+
+# Set the window size to cover the entire screen
+window.geometry(f"{window.winfo_screenwidth()}x{window.winfo_screenheight()}+0+0")
+
+# Create a title label with a big font
+title_label = tk.Label(window, text="Enter SQL Query", font=("Helvetica", 15))
+title_label.pack(pady=10)
+
+# Create an entry field for the SQL query
+sql_entry = tk.Entry(window, width=50, font=("Helvetica", 10))
+sql_entry.pack(padx=10, pady=10)
+
+# Legend items
+legend_items = [
+    {"text": "Start-up cost is the estimated Postgres computational units to start up a node to start process the query"},
+    {"text": "Total cost is the estimated Postgres computational units to finish process the query and return results"},
+    {"text": "Shared Hit Blocks: number of shared blocks read into cache"},
+    {"text": "Local Hit Blocks: number of local blocks read into cache"},
+]
+
+# Create a button to execute the SQL query
 def execute_sql_query():
+    global legend_canvas  # Declare legend_canvas as a global variable
     query = sql_entry.get()
 
     try:
@@ -27,46 +51,32 @@ def execute_sql_query():
         qep_label.image = qep_image
         qep_label.pack()
 
+        # Create a legend canvas
+        legend_canvas_height = len(legend_items) * 40 + 10  # Adjust the height based on the number of items
+        legend_canvas = tk.Canvas(window, width=700, height=legend_canvas_height)
+        legend_canvas.pack(padx=10, pady=10)
+
+        # Create legend items with font
+        for i, item in enumerate(legend_items):
+            y_offset = i * 40  # Adjust Y-coordinate to separate items
+            legend_canvas.create_oval(5, 5 + y_offset, 15, 15 + y_offset, fill="black")
+            legend_canvas.create_text(20, 10 + y_offset, text=item["text"], anchor="w", font=("Helvetica", 10))
+
     except Exception as e:
         result_label.config(text=f"Error: {str(e)}")
 
-    # Create a legend canvas
-    legend_canvas = tk.Canvas(window, width=600, height=100)
-    legend_canvas.pack(padx=10, pady=10)
-
-    # Legend items
-    legend_items = [
-        {"text": "start-up cost is the estimated Postgres computational units to start up a node to start process the query."},
-        {"text": "total cost is the estimated Postgres computational units to finish process the query and return results"},
-        {"text": "Shared Hit Blocks: number of shared blocks read into cache"},
-        {"text": "Local Hit Blocks: number of local blocks read into cache"},
-    ]
-
-    # Create four legend items
-    for i, item in enumerate(legend_items):
-        y_offset = i * 40  # Adjust Y-coordinate to separate items
-        legend_canvas.create_oval(5, 5 + y_offset, 15, 15 + y_offset, fill="black")
-        legend_canvas.create_text(20, 10 + y_offset, text=item["text"], anchor="w")
-
-# Create the main window
-window = tk.Tk()
-window.title("SQL Query Executor")
-
-# Create an entry field for the SQL query
-sql_entry = tk.Entry(window, width=50)
-sql_entry.pack(padx=10, pady=10)
-
 # Create a button to execute the SQL query
-execute_button = tk.Button(window, text="Execute Query", command=execute_sql_query)
+execute_button = tk.Button(window, text="Execute Query", command=execute_sql_query, font=("Helvetica", 10))
 execute_button.pack(padx=10, pady=10)
 
 # Create a label to display the QEP image
-qep_label = tk.Label(window)
+qep_label = tk.Label(window, font=("Helvetica", 12))
 qep_label.pack()
 
 # Create a label to display the result
-result_label = tk.Label(window, text="")
+result_label = tk.Label(window, text="", font=("Helvetica", 12))
 result_label.pack(padx=10, pady=10)
 
 # Start the mainloop
 window.mainloop()
+
