32d9fd9314fe71834dd099139aa61749e886315a
Lee Ding Zheng <101788791+leedz31@users.noreply.github.com>
2023-11-15 16:53:13 +0800

    added scrolling function to left and right canvas

--- project_2/main.py
+++ project_2/main.py
@@ -7,7 +7,7 @@ window = tk.Tk()
 window.title("SQL Query Executor")
 
 # Set the window size to cover the entire screen
-window.geometry(f"{window.winfo_screenwidth()}x{window.winfo_screenheight()}+0+0")
+# window.geometry(f"{window.winfo_screenwidth()}x{window.winfo_screenheight()}+0+0")
 
 # Create a top canvas for title, entry field, and button
 top_canvas = tk.Canvas(window)
@@ -20,6 +20,37 @@ title_label.pack()
 # Create an entry field for the SQL query in the top canvas
 sql_entry = tk.Entry(top_canvas, width=50, font=("Helvetica", 10))
 sql_entry.pack(pady=10)
+
+# Function to create scrollable canvas
+def create_scrollable_canvas(parent, side=tk.LEFT, padx=10, pady=10, min_width=400):
+    canvas = tk.Canvas(parent, width=min_width)
+    scrollbar = tk.Scrollbar(parent, orient="vertical", command=canvas.yview)
+    canvas.configure(yscrollcommand=scrollbar.set)
+    canvas.pack(side=side, fill="both", expand=True, padx=padx, pady=pady)
+    scrollbar.pack(side="right", fill="y")
+    frame = tk.Frame(canvas, width=min_width)
+    canvas_frame = canvas.create_window((min_width/2, 0), window=frame, anchor='center')
+    frame.bind("<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
+
+    def on_canvas_configure(event):
+        canvas.itemconfig(canvas_frame, x=event.width/2)
+
+    canvas.bind("<Configure>", on_canvas_configure)
+    return canvas, frame
+
+#Function to generate legend items on legend canvas
+def create_legend_items(canvas, legend_items, start_x, start_y, line_height, text_width, font_size):
+    bullet_diameter = 4
+    bullet_radius = bullet_diameter // 2
+    text_vertical_offset = font_size // 4
+    for i, item in enumerate(legend_items):
+        y_offset = start_y + i * line_height  # Adjust Y-coordinate to separate items
+        if( i < 2):
+            canvas.create_oval(start_x - bullet_radius, y_offset + text_vertical_offset - bullet_radius, start_x + bullet_radius, y_offset + text_vertical_offset + bullet_radius, fill="black")
+            canvas.create_text(start_x + 10, y_offset + 10, text=item["text"], anchor="w", font=("Helvetica", 10), width=text_width)
+        else:
+            canvas.create_oval(start_x - bullet_radius, y_offset + 10 - bullet_radius, start_x + bullet_radius, y_offset + 10 + bullet_radius, fill="black")
+            canvas.create_text(start_x + 10, y_offset + 10, text=item["text"], anchor="w", font=("Helvetica", 10), width=text_width)
 
 # Function to execute the SQL query
 def execute_sql_query():
@@ -51,20 +82,27 @@ def execute_sql_query():
 
         qep_label.config(image=qep_image)
         qep_label.image = qep_image
+        qep_label.pack(side="top", fill="both", expand=True)
 
-        # Update the statements in the right canvas
+        # Update the statements in the right frame
         analysis_output_label.config(text='\n'.join(statements))
+        analysis_output_label.pack(side="top", fill="both", expand=True)
 
-        # Create a legend canvas
+        # Create a legend canvas in the left frame
+        legend_canvas_width = 400
         legend_canvas_height = len(legend_items) * 40 + 10  # Adjust the height based on the number of items
-        legend_canvas = tk.Canvas(left_canvas, width=300, height=legend_canvas_height)
+        font_size = 10
+        legend_canvas = tk.Canvas(left_frame, width=legend_canvas_width, height=legend_canvas_height)
         legend_canvas.pack(pady=10)
+        
+        #Create legend items
+        create_legend_items(legend_canvas, legend_items, 20, 10, 40, legend_canvas_width - 30, font_size)
 
         # Create legend items with font
-        for i, item in enumerate(legend_items):
-            y_offset = i * 40  # Adjust Y-coordinate to separate items
-            legend_canvas.create_oval(5, 5 + y_offset, 15, 15 + y_offset, fill="black")
-            legend_canvas.create_text(20, 10 + y_offset, text=item["text"], anchor="w", font=("Helvetica", 10))
+        # for i, item in enumerate(legend_items):
+        #     y_offset = i * 40  # Adjust Y-coordinate to separate items
+        #     legend_canvas.create_oval(5, 5 + y_offset, 15, 15 + y_offset, fill="black")
+        #     legend_canvas.create_text(20, 10 + y_offset, text=item["text"], anchor="w", font=("Helvetica", 10))
 
     except Exception as e:
         result_label.config(text=f"Error: {str(e)}")
@@ -74,19 +112,24 @@ execute_button = tk.Button(top_canvas, text="Execute Query", command=execute_sql
 execute_button.pack(pady=10)
 
 # Create a left canvas for QEP image and legend items
-left_canvas = tk.Canvas(window)
-left_canvas.pack(side=tk.LEFT, padx=10, pady=10)
+# left_canvas = tk.Canvas(window)
+# left_canvas.pack(side=tk.LEFT, padx=10, pady=10)
 
 # Create a right canvas for QEP analysis output
-right_canvas = tk.Canvas(window)
-right_canvas.pack(side=tk.RIGHT, padx=10, pady=10, fill=tk.Y)
+# right_canvas = tk.Canvas(window)
+# right_canvas.pack(side=tk.RIGHT, padx=10, pady=10, fill=tk.Y)
+
+# Create scrollable left and right canvases
+left_canvas, left_frame = create_scrollable_canvas(window, side=tk.LEFT, min_width=400)
+right_canvas, right_frame = create_scrollable_canvas(window, side=tk.RIGHT, min_width=400)
 
 # Create a label to display the QEP analysis output in the right canvas
-analysis_output_label = tk.Label(right_canvas, text="", font=("Helvetica", 12), justify=tk.LEFT)
+analysis_output_label = tk.Label(right_frame, text="", font=("Helvetica", 12), justify=tk.LEFT)
 analysis_output_label.pack()
 
 # Legend items
 legend_items = [
+
     {"text": "Start-up cost is the estimated Postgres computational units to start up a node to start process the query."},
     {"text": "Total cost is the estimated Postgres computational units to finish process the query and return results"},
     {"text": "Shared Hit Blocks: number of shared blocks read into cache"},
@@ -94,11 +137,11 @@ legend_items = [
 ]
 
 # Create a label to display the QEP image in the left canvas
-qep_label = tk.Label(left_canvas, font=("Helvetica", 12))
+qep_label = tk.Label(left_frame, font=("Helvetica", 12))
 qep_label.pack()
 
 # Create a label to display the result in the left canvas
-result_label = tk.Label(left_canvas, text="", font=("Helvetica", 12))
+result_label = tk.Label(left_frame, text="", font=("Helvetica", 12))
 result_label.pack(pady=10)
 
 # Start the mainloop
